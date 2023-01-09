302a5ff92055ef332f65481ae4d1285cf4adb0dd
glendagarcia99 <jeanmilletteg99@gmail.com>
2023-01-09 12:09:44 -0600

    Modifcando rutas en el archivo

--- app-Peliculas/routes/web.php
+++ app-Peliculas/routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/pelicula', function () {
+    return view('pelicula.index');
+});
